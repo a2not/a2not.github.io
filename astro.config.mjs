d5fb02c69db575f8afc21ad9d0a174099c616ef8
a2not <a2not.dev@gmail.com>
2025-02-11 11:42:27 +0900

    add ga4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from '@astrojs/tailwind';
@@ -9,6 +10,16 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://a2not.github.io',
-    integrations: [mdx(), sitemap(), tailwind(), react()],
+  site: 'https://a2not.github.io',
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
